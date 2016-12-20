bc22dce9e57546fb292c4e9581256125b9659d4a
Daniel <daniel.ortizp91@gmail.com>
2016-12-19 19:02:46 -0600

    Now openpaysync is faster but less maintainable. WORTH IT

--- django_openpay/management/commands/openpaysync.py
+++ django_openpay/management/commands/openpaysync.py
@@ -25,7 +25,16 @@ class Command(BaseCommand):
                         openpay_id=planJson['id'])
                 finally:
                     dbobj.skip_signal = True
-                    dbobj.pull(commit=True)
+                    dbobj.name = planJson['name']
+                    dbobj.amount = Decimal(planJson['amount'])
+                    dbobj.status_after_retry = planJson['status_after_retry']
+                    dbobj.retry_times = planJson['retry_times']
+                    dbobj.repeat_unit = planJson['repeat_unit']
+                    dbobj.trial_days = planJson['trial_days']
+                    dbobj.repeat_every = planJson['repeat_every']
+                    dbobj.creation_date = parse_datetime(
+                        planJson['creation_date'])
+                    dbobj.save()
                     dbPlans[planJson['id']] = dbobj.pk
         print('{} plans pulled from Openpay.'.format(
             plansList.get('count', 0)))
@@ -38,15 +47,21 @@ class Command(BaseCommand):
             for cardJson in cardsList.get('data', []):
                 try:
                     dbobj = djop.models.Card.objects.get(
-                        openpay_id=cardJson['id'],
-                    )
+                        openpay_id=cardJson['id'])
                 except djop.models.Card.DoesNotExist:
                     dbobj = djop.models.Card(
-                        openpay_id=cardJson['id'],
-                        customer_id=customer.pk)
+                        openpay_id=cardJson['id'])
                 finally:
                     dbobj.skip_signal = True
-                    dbobj.pull(commit=True)
+                    dbobj.customer_id = customer.pk
+                    dbobj.card_type = cardJson['type']
+                    dbobj.holder = cardJson['holder_name']
+                    dbobj.number = cardJson['card_number'][-4:]
+                    dbobj.month = cardJson['expiration_month'][-2:]
+                    dbobj.year = cardJson['expiration_year'][-2:]
+                    dbobj.creation_date = parse_datetime(
+                        cardJson['creation_date'])
+                    dbobj.save()
                     dbCards[cardJson['id']] = dbobj.pk
         return dbCards
 
@@ -59,14 +74,26 @@ class Command(BaseCommand):
                         openpay_id=subscriptionJson['id'])
                 except djop.models.Subscription.DoesNotExist:
                     dbobj = djop.models.Subscription(
-                        openpay_id=subscriptionJson['id'],
-                        customer_id=customer.pk,
-                        plan_id=plans[subscriptionJson['plan_id']],
-                        card_id=cards[subscriptionJson['card']['id']]
-                    )
+                        openpay_id=subscriptionJson['id'])
                 finally:
                     dbobj.skip_signal = True
-                    dbobj.pull(commit=True)
+                    dbobj.customer_id = customer.pk,
+                    dbobj.plan_id = plans[subscriptionJson['plan_id']],
+                    dbobj.card_id = cards[subscriptionJson['card']['id']]
+                    dbobj.cancel_at_period_end = \
+                        subscriptionJson['cancel_at_period_end']
+                    dbobj.charge_date = parse_date(
+                        subscriptionJson['charge_date'])
+                    dbobj.period_end_date = parse_date(
+                        subscriptionJson['period_end_date'])
+                    dbobj.status = subscriptionJson['status']
+                    dbobj.current_period_number = \
+                        subscriptionJson['current_period_number']
+                    dbobj.trial_end_date = parse_date(
+                        subscriptionJson['trial_end_date'])
+                    dbobj.creation_date = parse_datetime(
+                        subscriptionJson['creation_date'])
+                    dbobj.save()
 
     def customers(self, plans):
         dbCustomers = dict()
