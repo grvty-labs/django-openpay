3245106d68f8590a0601bbe31e46672ca970d39f
Daniel <daniel.ortizp91@gmail.com>
2016-12-20 17:59:43 -0600

    BUmp version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='django-openpay',
-    version='1.0.0',
+    version='1.0.1',
     description='Django application which integrates the \
 OpenPay libraries for online transactions',
     long_description=readme,
